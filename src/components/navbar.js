cbcfdd21877a66574261a85e009c35be34ae0e08
shaharima Islam <shaharima@gmail.com>
2022-01-11 23:51:01 -0600

    Jan 12,2022 HW

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ const Navbar=props=>{
 
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand" to={"#"}>Navbar w/ text</Link>
+  <Link className="navbar-brand" to={"/"}>Navbar w/ text</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
